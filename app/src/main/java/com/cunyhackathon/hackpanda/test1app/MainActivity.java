b3ef746a9ca5e96360322b2d08f3b04e65956398
Frankie Ta <ohitzfrankie@gmail.com>
2017-05-01 16:50:29 -0400

    Initial commit

---
+++ app/src/main/java/com/cunyhackathon/hackpanda/test1app/MainActivity.java
@@ -0,0 +1,172 @@
+package com.cunyhackathon.hackpanda.test1app;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    SurfaceView cameraView;
+    TextView textView;
+    CameraSource cameraSource;
+    final int RequestCameraPermissionID = 1001;
+
+//  Method checks to see if permissions are granted by user.
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case RequestCameraPermissionID: {
+
+                // Condition checks to see if permission is granted by the user so that application can
+                // get access to the Camera.
+
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // If condition not true, means that permission was not granted,
+                    // then it will exit this method.
+
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    try {
+                        cameraSource.start(cameraView.getHolder()); // Camera source gets access from the SurfaceView used to allow Camera functionality.
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+            break;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraView = (SurfaceView) findViewById(R.id.surface_view);
+        textView = (TextView) findViewById(R.id.text_view);
+
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        if (!textRecognizer.isOperational()) {
+            Log.w("MainActivity", "Detector dependencies are not yet available");
+        } else {
+
+            cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setRequestedFps(2.0f)
+                    .setAutoFocusEnabled(true)
+                    .build();
+            cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+                    try {
+                        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.CAMERA},
+                                    RequestCameraPermissionID);
+                            return;
+                        }
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                    cameraSource.stop();
+                }
+            });
+
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                @Override
+                public void release() {
+
+                }
+
+                @Override
+                public void receiveDetections(Detector.Detections<TextBlock> detections) {
+
+                    final SparseArray<TextBlock> items = detections.getDetectedItems();
+                    if(items.size() != 0)
+                    {
+                        textView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                StringBuilder stringBuilder = new StringBuilder();
+                                for(int i =0;i<items.size();++i)
+                                {
+                                    TextBlock item = items.valueAt(i);
+                                    stringBuilder.append(item.getValue());
+                                    stringBuilder.append("\n");
+
+                                }
+                                textView.setText(stringBuilder.toString());         //String captured by the Camera
+
+                                final String str = stringBuilder.toString();        //String then stored in a new variable
+
+                                textView.setOnClickListener(new View.OnClickListener(){
+                                    public  void onClick(View v){
+
+
+                                        // When string is tapped, it will link to the dialer of the Phone
+                                        // application and output the phone number that is stored in the string variable.
+
+                                            Intent intent = new Intent(Intent.ACTION_DIAL);
+                                            intent.setData(Uri.parse("tel:" + str));
+                                            startActivity(intent);
+
+                                        // When string is tapped, it will link to the default Email
+                                        // application and output the email that is stored in the string variable.
+
+                                            Intent intent1 = new Intent(Intent.ACTION_SEND);
+                                            intent1.setData(Uri.parse("mailto:" + str));
+                                            startActivity(intent1);
+
+                                        // When string is tapped, it will link to the default Browser
+                                        // application and output the link stored in the string variable.
+
+                                            Intent intent2 = new Intent(Intent.ACTION_VIEW, Uri.parse(str));
+                                            startActivity(intent2);
+
+                                    }
+                                });
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    }
+}
+
+// UPDATED ON 4/29/17 10:39PM
